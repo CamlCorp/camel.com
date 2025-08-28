88db61bfa2e5c25fcd7dba789a3d5de4edfb4d68
omarmhmmd <omar.mhmmd@gmail.com>
2025-08-27 22:58:47 -0700

    styled

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
 ["src/pages/work/entries/bamyan.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fbamyan.mdx&astroContentModuleFlag=true")],
+["src/pages/work/entries/jainamaaz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fjainamaaz.mdx&astroContentModuleFlag=true")],
 ["src/pages/work/entries/flagsofafghanistan.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fflagsofafghanistan.mdx&astroContentModuleFlag=true")],
 ["src/pages/work/entries/millie-rail.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fmillie-rail.mdx&astroContentModuleFlag=true")],
-["src/pages/work/entries/jainamaaz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fjainamaaz.mdx&astroContentModuleFlag=true")],
 ["src/pages/work/entries/senj.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Fsenj.mdx&astroContentModuleFlag=true")],
 ["src/pages/work/entries/theamericanmosque.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fwork%2Fentries%2Ftheamericanmosque.mdx&astroContentModuleFlag=true")]]);
 		
